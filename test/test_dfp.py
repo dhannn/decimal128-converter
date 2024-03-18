c83d531f99f83737449ba75663b8fcfd41b4832a
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-03-18 15:05:13 +0800

    Add instruction for unit testing of Decimal Floating Point

---
+++ test/test_dfp.py
@@ -0,0 +1,6 @@
+""" Generate test cases for the DecimalFloatingPoint class
+
+Make sure to have good coverage (scenarios should span different cases
+such as different rounding methods, positive, negative, largest possible,
+smallest possible, zeroes, ...)
+"""
