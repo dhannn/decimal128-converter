82d8020a54c7d65389e1bd662e58976e42842a9a
oodrea <rjaemi@gmail.com>
2024-03-20 12:22:36 +0800

    Implemented DPD.py and test_dpd.py

--- src/utils/dpd.py
+++ src/utils/dpd.py
@@ -1,6 +1,6 @@
 from bitarray import bitarray
 
-from utils.bcd import BCD
+from src.utils.bcd import BCD
 
 class DPD:
     
@@ -23,6 +23,7 @@ class DPD:
 
     def __init__(self, val: int | str):
         bcd_value = BCD(val)
+        self.original_value = val
 
         bit_components = {
             chr(component): 
@@ -30,15 +31,19 @@ class DPD:
             for component in range(ord('a'), ord('l') + 1)
         }
 
-        aei = bitarray([bit_components['a'], bit_components['e'], bit_components['i']]).to01()
-        print(aei)
-        print(self.output_mapping[aei](bit_components))
+        aei = bitarray([bit_components['a'], bit_components['e'], bit_components['i']]).to01() # extract msb of each 4 bits
+
+        # For checking:
+        # print("val", self.original_value)
+        # print("aei: ", aei)
+        # print("densely packed bcd: ", self.output_mapping[aei](bit_components))
         
-        # TODO: Finish the implementation
-        output_bits = bitarray(12)
+        output_bits = self.output_mapping[aei](bit_components)
+        self.densely_packed = output_bits
 
     @staticmethod
     def extract_bit(bits: bitarray, char_index: str):
         index = DPD.input_char_to_index[char_index]
         padded_bits = bitarray(bits.to01().zfill(12))
         return padded_bits[index]
+    
--- test/test_dpd.py
+++ test/test_dpd.py
@@ -2,8 +2,7 @@ import unittest
 
 from bitarray import bitarray
 
-from utils.dpd import DPD
-
+from src.utils.dpd import *
 
 class TestDPD(unittest.TestCase):
 
@@ -12,8 +11,6 @@ class TestDPD(unittest.TestCase):
     def test_extract_bit__extract_start_triplet(self):
         expected = 1
         actual = DPD.extract_bit(self.bitarray_input, 'a')
-
-        print(self.bitarray_input)
 
         self.assertEqual(expected, actual,
                          f'Expected is { expected }, returns { actual }')
@@ -29,6 +26,46 @@ class TestDPD(unittest.TestCase):
     def test_extract_bit__extract_end_triplet(self):
         expected = 1
         actual = DPD.extract_bit(self.bitarray_input, 'l')
+
+        self.assertEqual(expected, actual,
+                         f'Expected is { expected }, returns { actual }')
+        
+    def test_987(self):
+        expected = bitarray('1110001111')
+        testdpd = DPD(987)
+        actual = testdpd.densely_packed
+
+        self.assertEqual(expected, actual,
+                         f'Expected is { expected }, returns { actual }')
+        
+    def test_007(self):
+        expected = bitarray('000 0000 111')
+        testdpd = DPD(7)
+        actual = testdpd.densely_packed
+
+        self.assertEqual(expected, actual,
+                         f'Expected is { expected }, returns { actual }')
+        
+    def test_256(self):
+        expected = bitarray('010 101 0 110')
+        testdpd = DPD(256)
+        actual = testdpd.densely_packed
+
+        self.assertEqual(expected, actual,
+                         f'Expected is { expected }, returns { actual }')
+        
+    def test_0(self):
+        expected = bitarray('000 000 0 000')
+        testdpd = DPD(0)
+        actual = testdpd.densely_packed
+
+        self.assertEqual(expected, actual,
+                         f'Expected is { expected }, returns { actual }')
+        
+    def test_52(self):
+        expected = bitarray('000 101 0 010')
+        testdpd = DPD(52)
+        actual = testdpd.densely_packed
 
         self.assertEqual(expected, actual,
                          f'Expected is { expected }, returns { actual }')
