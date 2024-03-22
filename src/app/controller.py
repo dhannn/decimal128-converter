6e1888a1dcc9f85f9000bc61647f3a63f4414381
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-03-22 22:30:36 +0800

    Add Save functionality and yassified GUI

--- src/app/controller.py
+++ src/app/controller.py
@@ -1,5 +1,5 @@
 from model import convert_to_decimal
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QHBoxLayout, QComboBox, QPushButton, QLayout, QPlainTextEdit
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QHBoxLayout, QComboBox, QPushButton, QMessageBox, QPlainTextEdit
 
 from src.utils.dfp import RoundingMethod
 
@@ -28,5 +28,17 @@ def convert(base_field: QLineEdit, exp_field: QLineEdit,
     bin_field.setPlainText(str(decimal128))
     hex_field.setPlainText(decimal128.to_hex())
 
-def save():
-    pass
+def save(base_field: QLineEdit, exp_field: QLineEdit, 
+         output_binary: QPlainTextEdit, output_hex: QPlainTextEdit):
+    base = base_field.text()
+    exp = exp_field.text()
+    bin_ = output_binary.toPlainText()
+    hex_ = output_hex.toPlainText()
+
+    with open(f'Decimal128_{ base }e{exp}.txt', 'w') as file:
+        file.write(f'Base-10:\t\t{base}e{exp}\nDecimal128:\t\t{bin_} (binary)\n\t\t\t\t{hex_} (hex)')
+    
+    message = QMessageBox()
+    message.setWindowTitle('Saved your output! :)')
+    message.setText(f'We saved your output to the text file, Decimal128_{ base }e{exp}.txt')
+    message.exec()
