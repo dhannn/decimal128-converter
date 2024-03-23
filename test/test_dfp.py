015cadd79fceb50f420f302deff8a9f97718fed4
Taco <bensonyao123@gmail.com>
2024-03-23 22:34:10 +0800

    Revert import in test_dfp

--- test/test_dfp.py
+++ test/test_dfp.py
@@ -1,8 +1,6 @@
 import unittest
 
 from bitarray import bitarray
-import os, sys
-sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]))
 from src.utils.dfp import *
 
 class TestDFP(unittest.TestCase):
