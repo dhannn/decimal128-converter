84ad937378b527d11cfc4161e1f7219d11f6baea
Taco <bensonyao123@gmail.com>
2024-03-23 20:42:58 +0800

    added positive normal and negative normal test case for DFP

--- test/test_dfp.py
+++ test/test_dfp.py
@@ -1,6 +1,28 @@
-""" TODO: Generate test cases for the DecimalFloatingPoint class
+import unittest
 
-Make sure to have good coverage (scenarios should span different cases
-such as different rounding methods, positive, negative, largest possible,
-smallest possible, zeroes, ...)
-"""
+from utils.dfp import *
+
+class TestDFP(unittest.TestCase):
+
+    """ TODO: Generate test cases for the DecimalFloatingPoint class
+
+    Make sure to have good coverage (scenarios should span different cases
+    such as different rounding methods, positive, negative, largest possible,
+    smallest possible, zeroes, ...)
+    """
+
+    def test_NormalPositive(self):
+        _input = 11_1234_5678_9012_3456_7890_1234_5678_9012
+        expected = bitarray('0_01001_0111_1101_1010_001_001_0010_011_100_0101_110_111_1000_001_000_1101_010_011_0100_101_110_0111_000_001_1110_001_010_0011_100_101_0110_111_100_1111_000_001_0010') 
+        actual = DecimalFloatingPoint(_input, -70, RoundingMethod.ROUND_TNE)
+
+        self.assertEqual(expected, actual,
+                    f'Expected is { expected }, returns { actual }')
+
+    def test_NormalNegative(self):
+        _input = -11_1234_5678_9012_3456_7890_1234_5678_9012
+        expected = bitarray('1_01001_0111_1101_1010_001_001_0010_011_100_0101_110_111_1000_001_000_1101_010_011_0100_101_110_0111_000_001_1110_001_010_0011_100_101_0110_111_100_1111_000_001_0010')
+        actual = DecimalFloatingPoint(_input, -70, RoundingMethod.ROUND_TNE)
+
+        self.assertEqual(expected, actual,
+                    f'Expected is { expected }, returns { actual }')
